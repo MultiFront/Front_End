135c9d99fc77093379bd517f84ac5b9b920cd08d
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-01-18 15:50:06 +0900

    mypage 관련 커밋, css추가

--- src/pages/mypage.jsx
+++ src/pages/mypage.jsx
@@ -1,93 +1,60 @@
 import React,{ useState }from 'react';
-import { Container, Row } from 'reactstrap'
+import { Col, Container, Row } from 'reactstrap'
 import '../styles/mypage.css'
 import Profile from './Mypage/Profile';
 import MatchedList from './Mypage/matchedlist';
 import BookedList from './Mypage/BookedList';
-import Resignmember from './Mypage/resignmember';
 import Inqury from './Mypage/inqury';
 import Favoritelist from './Mypage/favoritelist';
 import { motion } from 'framer-motion';
+import MypageMenu from '../assets/data/MypageMenu';
+import Resignmember from './Mypage/resignmember';
 
 
 const Mypage = () => {
 
-  const MypageMenu = [
-    {
-      id: 1,
-      text: '프로필',
-      name: 'MyProfile',
-      },
-      {
-      id: 2,
-      text: '매치내역',
-      name: 'MatchedList',
-      },
-      {
-      id: 3,
-      text: '예약내역',
-      name: 'BookedList',
-      },
-      {
-      id: 4,
-      text: '내가찜한목록',
-      name: 'FavoriteList',
-      },
-      {
-      id: 5,
-      text: '1:1문의 내역',
-      name: 'Inqury',
-      },
-      {
-      id: 6,
-      text: '회원탈퇴',
-      name: 'ResignMember',
-      },
-  ];
 
-
-  function Hi(){
-    return(
-      <Profile />
-    )
-  }
-
-  const [content, setContent] = useState(() => Hi());
-
-  const handleClickButton = e => {
-      const { name } = e.target;
-      setContent(name);
-  };
-  
 
   const selectComponent = {
-    MyProfile: <Profile />,
-    MatchedList: <MatchedList/>,
-    BookedList: <BookedList />,
-    FavoriteList: <Favoritelist />,
-    Inqury: <Inqury />,
-    ResignMember:  <Resignmember />,
+    0: <Profile />,
+    1: <MatchedList/>,
+    2: <BookedList />,
+    3: <Favoritelist />,
+    4: <Inqury />,
+    5: <Resignmember/>,
   };
+
+
+  const [content, setContent] = useState(0);
+
+  const handleClickButton = (index) => {
+      setContent(index);
+  };
+
 
   return (
     <Container>
       <Row className='m40'>
+        <Col lg='2' >
         <div className='mypage__list'>
-        {MypageMenu.map(data => {
+        {MypageMenu.map((el,index) => {
             return (
-              <motion.button whileTap={{scale:1.1}} onClick={handleClickButton} name={data.name} key={data.id}>
-                {data.text}
+              <motion.button className={content === index ? "button--on" : "button--off"} whileTap={{scale:1.1}} onClick={()=>handleClickButton(index)} name={el.name} key={el.id}>
+                {el.text}
               </motion.button>
             );
           })}
         </div>
-
-
-         {content || <div>{selectComponent[content]}</div>}
-
+        </Col>
+        <Col  lg='10' >
+           {
+            selectComponent[content] 
+           }
+        </Col>
       </Row>
     </Container>
   );
 }
 
 export default Mypage
+
