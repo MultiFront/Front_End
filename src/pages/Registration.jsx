966c8613fef7d034f9a2f4af5c8970c36b695b2b
KAI <a90605190@gmail.com>
2023-01-23 22:34:02 +0900

    Reg mod

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import '../styles/registration.css';
 
 function Registration() {
   return (
-    <div>
-      Hello World!
+    <div className='regWrap'>
+      <div className='regMain'>
+        <div className='regBanner'>매칭을 직접 만들어 보세요!</div>
+        <div className='regTitle'>
+          <h3>매칭 제목을 등록해 주세요</h3>
+          <input type="text" placeholder='제목을 입력하시오.'/>
+        </div>
+        <div className='regSelector'>
+          <select name="" id="">
+            <option value="">종목</option>
+            <option value="">풋살</option>
+            <option value="">수영</option>
+            <option value="">테니스</option>
+            <option value="">배드민턴</option>
+            <option value="">탁구</option>
+          </select>
+          <select name="" id="">
+            <option value="">레벨</option>
+            <option value="">초급</option>
+            <option value="">중급</option>
+            <option value="">상급</option>
+          </select>
+        </div>
+
+      </div>
     </div>
   )
 }
