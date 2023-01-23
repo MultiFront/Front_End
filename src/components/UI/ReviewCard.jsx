cf64185390e78ef64dc482f87f8e896bcab651db
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-01-24 00:32:50 +0900

    review Card

--- src/components/UI/ReviewCard.jsx
+++ src/components/UI/ReviewCard.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import {motion} from 'framer-motion';
+import Modal from '../UI/Modal';
+import ReviewForm from '../../pages/Mypage/ReviewForm';
+import '../../styles/modal.css'
 
-const ReviewCard = () => {
+
+const ReviewCard = (props) => {
+
+  const [reviewForm, setReviewForm] = useState(false);
+
+
   return (
+    
     <Col lg='3' md='4' className='mb-20'>
     <div>
       <div className="match-list">
@@ -16,9 +25,19 @@ const ReviewCard = () => {
           <p className="match-location">2023년 1월 18일</p>
 
 
-          <motion.button whileHover={{scale:1.1}} className='review__btn'><i class="ri-edit-line"></i>후기작성 하기</motion.button>
+          <motion.button whileHover={{scale:1.1}} className='review__btn'  onClick={() => setReviewForm(!reviewForm)} >
+            <i class="ri-edit-line"></i>후기작성 하기
+          </motion.button>
+
+          {reviewForm && (
+          <Modal closeModal={() => setReviewForm(!reviewForm)}>
+            <ReviewForm />
+          </Modal>
+          )}
       </div>
     </div>
+
+   
     </Col>
   )
 }
