0df5fff940472fe4dde414062cd1806785854d9e
Zoey425 <108717962+Zoey425@users.noreply.github.com>
2023-01-25 13:51:08 +0900

    EY MYPAGE, HOME COMMIT

--- src/pages/LessonDetails.jsx
+++ src/pages/LessonDetails.jsx
@@ -9,11 +9,6 @@ import Comment from './MatchingDetail/comment'
 import MainBanner from './MatchingDetail/MainBanner'
 import Favorite from '../components/UI/favorite'
 import { UncontrolledCarousel } from 'reactstrap';
-import {
-  Pagination,
-  PaginationItem,
-  PaginationLink,
-} from "reactstrap";
 
 const LessonDetails = () => {
   return (
@@ -31,27 +26,27 @@ const LessonDetails = () => {
                 <h5 style={{marginBottom: '30px'}}>작성자(닉네임)</h5>
 
                 <UncontrolledCarousel style={{marginBottom:'20px'}}
-                items={[
-                  {
-                    altText: '',
-                    caption: '',
-                    key: 1,
-                    src: 'https://picsum.photos/id/123/1200/600'
-                  },
-                  {
-                    altText: '',
-                    caption: '',
-                    key: 2,
-                    src: 'https://picsum.photos/id/456/1200/600'
-                  },
-                  {
-                    altText: '',
-                    caption: '',
-                    key: 3,
-                    src: 'https://picsum.photos/id/678/1200/600'
-                  }
-                ]}
-              />
+                  items={[
+                    {
+                      altText: '',
+                      caption: '',
+                      key: 1,
+                      src: 'https://picsum.photos/id/123/1200/600'
+                    },
+                    {
+                      altText: '',
+                      caption: '',
+                      key: 2,
+                      src: 'https://picsum.photos/id/456/1200/600'
+                    },
+                    {
+                      altText: '',
+                      caption: '',
+                      key: 3,
+                      src: 'https://picsum.photos/id/678/1200/600'
+                    }
+                  ]}
+                />
 
                 <MatchCategory category='참가비'/>
                 <MatchCategory category='상세정보'/>
@@ -74,70 +69,7 @@ const LessonDetails = () => {
                     <Comment/>
                     <Comment/>
                 </div>
-                <nav className='Page' aria-label="Page navigation example">
-                <Pagination>
-                  <PaginationItem>
-                    <PaginationLink className='pageSingle' style={{borderRadius: '20px', border: '2px solid #6bd098', color:'#6bd098'}}
-                      aria-label="Previous"
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      <i aria-hidden={true} className="fa fa-angle-left" />
-                      <span className="sr-only">Previous</span>
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink className='pageSingle' style={{borderRadius: '20px', border: '2px solid #6bd098', color:'#6bd098'}}
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      1
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink className='pageSingle' style={{borderRadius: '20px', border: '2px solid #6bd098', color:'#6bd098'}}
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      2
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink className='pageSingle' style={{borderRadius: '20px', border: '2px solid #6bd098', color:'#6bd098'}}
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      3
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink className='pageSingle' style={{borderRadius: '20px', border: '2px solid #6bd098', color:'#6bd098'}}
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      4
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink className='pageSingle' style={{borderRadius: '20px', border: '2px solid #6bd098', color:'#6bd098'}}
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      5
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink className='pageSingle' style={{borderRadius: '20px', border: '2px solid #6bd098', color:'#6bd098'}}
-                      aria-label="Next"
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      <i aria-hidden={true} className="fa fa-angle-right" />
-                      <span className="sr-only">Next</span>
-                    </PaginationLink>
-                  </PaginationItem>
-                </Pagination>
-              </nav>
+
                 </div>
               </main>
             </div>
